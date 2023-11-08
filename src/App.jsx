352e5d7599b94bc55ce63d8525c049a7ed327c55
Diego <70916554+Diegosaj@users.noreply.github.com>
2023-11-08 18:01:03 -0300

    agregado de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar/Navbar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 
 function App () {
